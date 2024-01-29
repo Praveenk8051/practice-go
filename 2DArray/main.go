405709822976444b32bdf4246da4aee66360380b
Praveenk8051 <praveenk8051@gmail.com>
2024-01-29 19:53:45 +0100

    Alter names

--- 2DArray/main.go
+++ 2DArray/main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var arr = [5][2]int{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
-	// var i,j int
-	fmt.Println(arr[0])
-	// for  i = 0; i < a[]; i++ {
-	// 	for j = 0; j < 2; j++ {
-	// 	   fmt.Printf("a[%d][%d] = %d\n", i,j, a[i][j] )
-	// 	}
-	//  }
-
+	var arr = [5][2]int{{1, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+	var arr1 [5][2]int
+	var i, j int
+	fmt.Println(arr[3][0])
+	for i = 0; i < 2; i++ {
+		for j = 0; j < 5; j++ {
+			arr1[j][i] = i * j
+		}
+	}
+	fmt.Println(arr1)
 }
