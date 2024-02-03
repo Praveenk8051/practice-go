64fd0e0799c299c0f1929a97c8ac30c93697baa1
Praveenk8051 <praveenk8051@gmail.com>
2024-02-03 10:46:58 +0100

    fix

--- 2DArray/main.go
+++ 2DArray/main.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var arr = [5][2]int{{1, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
-	var arr1 [5][2]int
+	var num1, num2 int
+	fmt.Scanf("%d,%d", &num1, &num2)
+	// var arr [num1][num2]int
+	// var arr1 [num1][num2]int
+	var arr1 [][]int
+	fmt.Println(arr1)
 	var i, j int
-	fmt.Println(arr[3][0])
-	for i = 0; i < 2; i++ {
-		for j = 0; j < 5; j++ {
-			arr1[j][i] = i * j
+	// fmt.Println(arr[0][0])
+	for i = 0; i < num2; i++ {
+		for j = 0; j < num1; j++ {
+			arr1[num2][num1] = i * j
+			fmt.Println(arr1)
 		}
 	}
 	fmt.Println(arr1)
