dfdf86799f083c4b9aa835a7eb91882a60d6839e
Praveenk8051 <praveenk8051@gmail.com>
2024-02-11 10:33:10 +0100

    removedups

--- RemoveDuplicates/removeduplicates.go
+++ RemoveDuplicates/removeduplicates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -18,16 +19,34 @@ func main() {
 	// Split the input string into individual binary numbers
 	manyStrings := strings.Split(seriesString, ",")
 	var arrString []string
-	for _, element := range manyStrings {
-		arrString = append(arrString, element)
-	}
-	fmt.Println(arrString)
+	arrString = append(arrString, manyStrings...)
+	// for _, element := range manyStrings {
+	// 	arrString = append(arrString, element)
+	// }
+	// fmt.Println(arrString)
+	result := removeDuplicates(arrString)
+	fmt.Println(result)
 	// sort.Slice(seriesString)
-	// sort.Slice(seriesString, func(i, j int) bool {
-	// 	return seriesString[i] < seriesString[j]
-	// })
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
+	})
+	fmt.Println(result)
+
 	// fmt.Println("Sorted alphanumerically:", seriesString)
 	// for _, manyString := range manyStrings {
 
 	// }
+}
+func removeDuplicates(elements []string) []string {
+	encountered := map[string]bool{} // Map to keep track of encountered elements
+	result := []string{}             // Slice to store unique elements
+
+	for _, v := range elements {
+		if encountered[v] == false {
+			encountered[v] = true
+			result = append(result, v)
+		}
+	}
+
+	return result
 }
