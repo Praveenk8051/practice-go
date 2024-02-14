40fe18f9179ea20a8d3a215ba5c7a2d3368c891d
Praveenk8051 <praveenk8051@gmail.com>
2024-02-14 17:53:13 +0100

    getting Better

--- RemoveDuplicates/removeduplicates.go
+++ RemoveDuplicates/removeduplicates.go
@@ -2,53 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
 func main() {
-	// var seriesString string
-	// fmt.Println("Enter the series of comma separated string")
-	// fmt.Scanf("%s", &seriesString)
-	// _, err := fmt.Scan(&seriesString)
-
-	// if err != nil {
-	// 	fmt.Println("Error reading input:", err)
-	// 	return
-	// }
-	seriesString := "aaa, aaa, bbb,ccc,aaa"
-	splitStrings := strings.Split(seriesString, ",")
-	// Trim whitespace from each string in the slice
-	for i := range splitStrings {
-		splitStrings[i] = strings.TrimSpace(splitStrings[i])
+	fmt.Println("Enter any comma seperated words")
+	var input string
+	_, err := fmt.Scanf("%s", &input)
+	if err != nil {
+		fmt.Println("Unknown Value")
+	} else {
+		output := removeDuplicates(input)
+		fmt.Print(output)
 	}
-	fmt.Println(splitStrings)
-	// var arrString []string
-	// aaa,aaa,bbb,ccc,aaa
-	// arrString = append(arrString, manyStrings...)
-	// for _, element := range manyStrings {
-	// 	arrString = append(arrString, element)
-	// }
-	// fmt.Println(arrString)
-	// result := removeDuplicates(arrString)
-	// fmt.Println(result)
-	// // sort.Slice(seriesString)
-	// sort.Slice(result, func(i, j int) bool {
-	// 	return result[i] < result[j]
-	// })
-	// fmt.Println(result)
-
 }
+func removeDuplicates(input string) string {
+	fmt.Println(input)
+	// make a map and split the input string
+	m := make(map[string]bool)
+	temp := strings.Split(input, " ")
 
-// func removeDuplicates(elements []string) []string {
-// 	encountered := map[string]bool{} // Map to keep track of encountered elements
-// 	result := []string{}             // Slice to store unique elements
+	// add values to map as keys (making them unique)
+	for _, v := range temp {
+		m[v] = true
+	}
 
-// 	for _, v := range elements {
-// 		if encountered[v] == false {
-// 			encountered[v] = true
-// 			result = append(result, v)
-// 		}
-// 	}
+	// convert map back into slice
+	var result []string
+	for key := range m {
+		result = append(result, key)
+	}
 
-// 	return result
-// }
+	// sort slice and join back toghether
+	sort.Strings(result)
+	output := strings.Join(result, " ")
+
+	return output
+}
