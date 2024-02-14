8eb4743b5865917b8b6ffcd599ead6d996bf61a4
Praveenk8051 <praveenk8051@gmail.com>
2024-02-14 17:41:44 +0100

    still not good

--- RemoveDuplicates/removeduplicates.go
+++ RemoveDuplicates/removeduplicates.go
@@ -2,50 +2,53 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 )
 
 func main() {
-	var seriesString string
-	fmt.Scanf("Enter the series of comma separated string %s", &seriesString)
-	_, err := fmt.Scan(&seriesString)
+	// var seriesString string
+	// fmt.Println("Enter the series of comma separated string")
+	// fmt.Scanf("%s", &seriesString)
+	// _, err := fmt.Scan(&seriesString)
 
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
+	// if err != nil {
+	// 	fmt.Println("Error reading input:", err)
+	// 	return
+	// }
+	seriesString := "aaa, aaa, bbb,ccc,aaa"
+	splitStrings := strings.Split(seriesString, ",")
+	// Trim whitespace from each string in the slice
+	for i := range splitStrings {
+		splitStrings[i] = strings.TrimSpace(splitStrings[i])
 	}
-
-	// Split the input string into individual binary numbers
-	seriesString = strings.Trim(seriesString, " ")
-	manyStrings := strings.Split(seriesString, ",")
-
-	var arrString []string
+	fmt.Println(splitStrings)
+	// var arrString []string
+	// aaa,aaa,bbb,ccc,aaa
 	// arrString = append(arrString, manyStrings...)
-	for _, element := range manyStrings {
-		arrString = append(arrString, element)
-	}
-	fmt.Println(arrString)
-	result := removeDuplicates(arrString)
-	fmt.Println(result)
-	// sort.Slice(seriesString)
-	sort.Slice(result, func(i, j int) bool {
-		return result[i] < result[j]
-	})
-	fmt.Println(result)
+	// for _, element := range manyStrings {
+	// 	arrString = append(arrString, element)
+	// }
+	// fmt.Println(arrString)
+	// result := removeDuplicates(arrString)
+	// fmt.Println(result)
+	// // sort.Slice(seriesString)
+	// sort.Slice(result, func(i, j int) bool {
+	// 	return result[i] < result[j]
+	// })
+	// fmt.Println(result)
 
 }
 
-func removeDuplicates(elements []string) []string {
-	encountered := map[string]bool{} // Map to keep track of encountered elements
-	result := []string{}             // Slice to store unique elements
+// func removeDuplicates(elements []string) []string {
+// 	encountered := map[string]bool{} // Map to keep track of encountered elements
+// 	result := []string{}             // Slice to store unique elements
 
-	for _, v := range elements {
-		if encountered[v] == false {
-			encountered[v] = true
-			result = append(result, v)
-		}
-	}
+// 	for _, v := range elements {
+// 		if encountered[v] == false {
+// 			encountered[v] = true
+// 			result = append(result, v)
+// 		}
+// 	}
 
-	return result
-}
+// 	return result
+// }
