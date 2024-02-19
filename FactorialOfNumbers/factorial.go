37761edab14716aad4c510e0901e7001ffc72f84
Krishna Murthy Praveen (AE/MFD1) <Praveen.KrishnaMurthy2@de.bosch.com>
2024-02-19 16:13:40 +0100

    review the tests.might be wrong

--- FactorialOfNumbers/factorial.go
+++ FactorialOfNumbers/factorial.go
@@ -5,15 +5,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Enter the number to find factorial")
 	var num int
-	_, err := fmt.Scanf("%d", &num)
-	if err != nil {
-		fmt.Println("Unknown value")
-	}
+	num = printNum(num)
 	fmt.Printf("You have entered %d\n", num)
 	result := factorial(num)
 	printResult(result, num)
+}
+func printNum(num int) int {
+	fmt.Println("Enter the number below 20, to find factorial")
+
+	_, err := fmt.Scanf("%d", &num)
+	checkifGreater(num, err)
+	return num
+}
+
+func checkifGreater(num int, err error) {
+	if err != nil {
+		fmt.Println("Unknown value")
+	}
+	if !(num < 20) {
+		printNum(num)
+	}
 }
 
 func factorial(num int) int {
---
+++ FactorialOfNumbers/factorial_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+
+	assert.Equal(t, int(16), int(16))
+}
