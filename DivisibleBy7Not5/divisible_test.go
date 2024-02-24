e6d64068fdbb5270af4be48ca02a6af047278262
Praveenk8051 <praveenk8051@gmail.com>
2024-02-24 11:03:13 +0100

    trying something new

--- DivisibleBy7Not5/divisible_test.go
+++ DivisibleBy7Not5/divisible_test.go
@@ -7,13 +7,16 @@ import (
 )
 
 func TestMain(t *testing.T) {
+	asserts := assert.New(t)
+	actual, err := DivisibleBy7(2100)
+	asserts.Nil(err)
+	asserts.Equal(false, actual)
+	// asserts.Equal(t, int(2002), int(2002))
+	// assert.Equal(t, 2009, int(2009))
+	// assert.Equal(t, 2023, int(2023))
+	// assert.Equal(t, 2051, int(2051))
 
-	assert.Equal(t, int(2002), int(2002))
-	assert.Equal(t, 2009, int(2009))
-	assert.Equal(t, 2023, int(2023))
-	assert.Equal(t, 2051, int(2051))
-
-	assert.NotEqual(t, 2024, "Number doesn't satify the needs")
-	assert.NotEqual(t, 2050, "Number doesn't satify the needs")
-	assert.NotEqual(t, 2055, "Number doesn't satify the needs")
+	// assert.NotEqual(t, 2024, "Number doesn't satify the needs")
+	// assert.NotEqual(t, 2050, "Number doesn't satify the needs")
+	// assert.NotEqual(t, 2055, "Number doesn't satify the needs")
 }
